36facc0bba0745fb9535d911b46ff83f7017a5c0
dimobr <d96obradovic@gmail.com>
2024-05-25 22:20:29 +0200

    Add a Sample Job Application API Controller.

---
+++ TrackJobApp/Controllers/JobApplicationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TrackJobApp.Models.DtoModels;
+using TrackJobApp.Services.Samples;
+
+namespace TrackJobApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobApplicationController : ControllerBase
+    {
+        private readonly ILogger<JobApplicationController> _logger;
+        private readonly IJobApplicationService _jobApplicationService;
+
+        public JobApplicationController(ILogger<JobApplicationController> logger,
+            IJobApplicationService jobApplicationService)
+        {
+            _logger = logger;
+            _jobApplicationService = jobApplicationService;
+        }
+
+        [HttpGet(Name = "GetJobApplications")]
+        public IEnumerable<JobApplicationDto> Get()
+        {
+            var jobApplications = _jobApplicationService.GetApplications();
+
+            return jobApplications.Select(jobApplicationEntity => new JobApplicationDto() 
+            {
+                Id = jobApplicationEntity.Id,
+                Name = jobApplicationEntity.PositionName,
+                ApplicationDate = jobApplicationEntity.ApplicationDate,
+                Status = jobApplicationEntity.StatusAsString,
+                Notes = jobApplicationEntity.Notes,
+                Description = jobApplicationEntity.Description?.DescriptionText
+            });
+        }
+    }
+}
